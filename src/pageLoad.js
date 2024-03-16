d0f92259f27074a6d5fe24218a6343528e1f655a
irizow <142471638+irizow@users.noreply.github.com>
2024-03-17 01:22:34 +0800

    Update pageLoad.js

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -60,8 +60,8 @@ function getWeatherCondition(response, loop, isHourly) {
 
 function getWeatherAssets(weatherCondition, isNight) {
   const basePath = {
-    backgrounds: "../src/images/backgrounds/",
-    icons: "../src/images/weather-icons-master/svg/",
+    backgrounds: "https://github.com/irizow/weather-app/tree/main/src/images/backgrounds/",
+    icons: "https://github.com/irizow/weather-app/tree/main/src/images/weather-icons-master/svg/",
   };
   const assets = {
     Rain: {
